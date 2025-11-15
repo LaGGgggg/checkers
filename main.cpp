508da3e6489ae63558ecc08d4f46f0704b16a5bf
LittelIrbis <postelnyhpolina@gmail.com>
2025-11-15 13:36:23 +0300

    add double jump in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Event.hpp>
 #include <iostream>
@@ -11,31 +12,59 @@
 #include <ctime>
 #include <cstdlib>
 
-// создание поля [x][y]
+bool at_home = false;
+
+
+// creating fied [y][x]
 mySqare sqare[8][8];
 
 int main() {
 
-	bool MY_COLOR = false;
-	int pos = MY_COLOR ? 5 : 0;  // чтобы снизу были шашки данного игрока, а сверху противника
+	
 
 
 	// window =========================================================================================================
 	sf::RenderWindow window(sf::VideoMode({ WINDOW_X, WINDOW_Y }), "Checks");
 
-
-	// Задний фон
+	// Background
 	sf::RectangleShape background({ WINDOW_X, WINDOW_Y });
 	background.setFillColor(back_color);
 
-	// Поле (белые клетки)
+	// Home screen ====================================================================================================
+
+	
+	sf::CircleShape white_example;
+	white_example.setRadius(CHECKER_RADIUS * 3);
+	white_example.setFillColor(wite_checker);
+	white_example.setOutlineThickness(-1);
+	white_example.setOutlineColor(white_sq);
+
+	white_example.setPosition({ WINDOW_X / 2 - 100, WINDOW_Y/2 - 100 });
+
+	sf::CircleShape black_example;
+	black_example.setRadius(CHECKER_RADIUS * 3);
+	black_example.setFillColor(black_checker);
+	black_example.setOutlineThickness(-1);
+	black_example.setOutlineColor(outline);
+
+	black_example.setPosition({ WINDOW_X/2 + 300, WINDOW_Y / 2 - 100 });
+
+	
+	
+	bool MY_COLOR = false;
+	int pos = MY_COLOR ? 5 : 0;  // С‡С‚РѕР±С‹ СЃРЅРёР·Сѓ Р±С‹Р»Рё С€Р°С€РєРё РґР°РЅРЅРѕРіРѕ РёРіСЂРѕРєР°, Р° СЃРІРµСЂС…Сѓ РїСЂРѕС‚РёРІРЅРёРєР°
+
+
+	// Field screen ===================================================================================================
+
+	// Field (white sqares)
 	sf::RectangleShape field({ FIELD_SIZE, FIELD_SIZE });
 	field.setFillColor(white_sq);
 	field.setPosition({ FIELD_OFFSET_X, FIELD_OFFSET_Y });
 	field.setOutlineThickness(3);
-	field.setOutlineColor(boulders);
+	field.setOutlineColor(outline);
 
-	// Черные клетки
+	// Black sqares
 	sf::RectangleShape sq[32];
 	for (int i = 0; i < 32; ++i) {
 
@@ -54,7 +83,7 @@ int main() {
 		}
 	}
 
-	// Белые шашки
+	// White checkers
 	sf::CircleShape w_checker[12];
 	for (int i = 0; i < 12; ++i) {
 
@@ -74,14 +103,14 @@ int main() {
 	}
 
 
-	// Черные шашки
+	// Black checkers
 	sf::CircleShape b_checker[12];
 	for (int i = 0; i < 12; ++i) {
 
 		b_checker[i].setRadius(CHECKER_RADIUS);
 		b_checker[i].setFillColor(black_checker);
 		b_checker[i].setOutlineThickness(-1);
-		b_checker[i].setOutlineColor(boulders);
+		b_checker[i].setOutlineColor(outline);
 
 	}
 	k = 0;
@@ -95,7 +124,7 @@ int main() {
 	}
 
 
-	// привязка шашек к клеткам
+	// binding checkers and sqares
 	k = 0;
 	for (int i = 0 + pos; i < 3 + pos; ++i) {                //y
 		for (int j = 0; j < 8; ++j) {                        //x
@@ -117,51 +146,79 @@ int main() {
 		}
 	}
 
+	//set color 
+	
 
 
 
 
-	while (window.isOpen()) { //    Работа с окном    =================================================================
+
+	while (window.isOpen()) { //    Window    =================================================================
 
 
-		while (const std::optional event = window.pollEvent()) {  // pollEvent - возвращает событие, если оно ожидает обработки
-			// закрытие окна
+		while (const std::optional event = window.pollEvent()) {  // pollEvent - РІРѕР·РІСЂР°С‰Р°РµС‚ СЃРѕР±С‹С‚РёРµ, РµСЃР»Рё РѕРЅРѕ РѕР¶РёРґР°РµС‚ РѕР±СЂР°Р±РѕС‚РєРё
+			
 			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
 
 
-			// нажатие на поле
+			// tape
 			if (const auto* mouseButton = event->getIf<sf::Event::MouseButtonPressed>()) {
 
 				int click_x = mouseButton->position.x;
 				int click_y = mouseButton->position.y;
 
-				sf::Vector2f point{
-					static_cast<float>(click_x),
-					static_cast<float>(click_y)
-				};
+				// calculate point if click
+				sf::Vector2f point{ static_cast<float>(click_x), static_cast<float>(click_y)}; 
 
 				if (field.getGlobalBounds().contains(point)) {
-					type(click_x, click_y, sq, sqare);
+
+					int x = (click_x - FIELD_OFFSET_X) / SQ_SIZE;
+					int y = (click_y - FIELD_OFFSET_Y) / SQ_SIZE;
+
+					type(x, y, sq, sqare);
 				}
+
+
+
+			}
+
+			
+			if (const auto* textEntered = event->getIf<sf::Event::TextEntered>()) {
+
+				if (textEntered -> unicode >= 0 && textEntered -> unicode <= 9)
+				std::cout << "ASCII character typed: " << static_cast<char>(textEntered->unicode) << std::endl;
+			
+
 			}
 		}
 
 
 
 		window.clear();
-
-		// отрисовка объектов ----------------------------------
 		window.draw(background);
-		window.draw(field);
-		for (int i = 0; i < 32; ++i) {
-			window.draw(sq[i]);
+		if (at_home) {
+
+			window.draw(white_example);
+			window.draw(black_example);
+
 		}
-		for (int i = 0; i < 12; ++i) {
-			window.draw(w_checker[i]);
-			window.draw(b_checker[i]);
+		else {
+
+			window.draw(field);
+			for (int i = 0; i < 32; ++i) {
+				window.draw(sq[i]);
+			}
+			for (int i = 0; i < 12; ++i) {
+				window.draw(w_checker[i]);
+				window.draw(b_checker[i]);
+			}
+
 		}
+
+
+		
 
 
 		window.display();
