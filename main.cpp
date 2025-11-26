1f8707213b648a48ad838053c4874814e971d14b
LittelIrbis <postelnyhpolina@gmail.com>
2025-11-26 20:32:28 +0300

    feat: возможность взаимодействовать через сокеты

--- main.cpp
+++ main.cpp
@@ -1,131 +1,100 @@
-#pragma once
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Event.hpp>
 #include <iostream>
-#include "storage.h"
 #include <SFML/Window.hpp>
 #include <optional>
-
 #include <SFML/Audio.hpp>
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
 
+
+#include "storage.h"
+#include "socket_manager.h"
+
 bool at_home = false;
+bool is_end = false;
+bool is_waiting_for = false; 
+int last_x, last_y;
 
 
-// creating fied [y][x]
-mySqare sqare[8][8];
+mySqare sqare[8][8]; // creating fied [y][x]
 
-int main() {
+void seting_home_screen(sf::CircleShape& white_example, sf::CircleShape& black_example);
+void seting_field(sf::RectangleShape& field, sf::RectangleShape sq[32]);
+void setting_chekers(int pos, sf::CircleShape w_checker[12], sf::CircleShape b_checker[12]);
 
-	
+int main(int argc, char* argv[]) {
 
-
-	// window =========================================================================================================
+	// window ==================================================================================================================================================================
 	sf::RenderWindow window(sf::VideoMode({ WINDOW_X, WINDOW_Y }), "Checks");
 
-	// Background
 	sf::RectangleShape background({ WINDOW_X, WINDOW_Y });
 	background.setFillColor(back_color);
 
-	// Home screen ====================================================================================================
 
-	
+
+	// Home screen =============================================================================================================================================================
 	sf::CircleShape white_example;
-	white_example.setRadius(CHECKER_RADIUS * 3);
-	white_example.setFillColor(wite_checker);
-	white_example.setOutlineThickness(-1);
-	white_example.setOutlineColor(white_sq);
-
-	white_example.setPosition({ WINDOW_X / 2 - 100, WINDOW_Y/2 - 100 });
-
 	sf::CircleShape black_example;
-	black_example.setRadius(CHECKER_RADIUS * 3);
-	black_example.setFillColor(black_checker);
-	black_example.setOutlineThickness(-1);
-	black_example.setOutlineColor(outline);
-
-	black_example.setPosition({ WINDOW_X/2 + 300, WINDOW_Y / 2 - 100 });
-
-	
-	
-	bool MY_COLOR = false;
-	int pos = MY_COLOR ? 5 : 0;  // чтобы снизу были шашки данного игрока, а сверху противника
+	seting_home_screen(white_example, black_example);
 
 
-	// Field screen ===================================================================================================
+	// Creating server / client
+	const bool is_client = (argc > 1 && std::string(argv[1]) == "client"); 
 
-	// Field (white sqares)
-	sf::RectangleShape field({ FIELD_SIZE, FIELD_SIZE });
-	field.setFillColor(white_sq);
-	field.setPosition({ FIELD_OFFSET_X, FIELD_OFFSET_Y });
-	field.setOutlineThickness(3);
-	field.setOutlineColor(outline);
+	SocketManager socket_manager_server;
+	SocketManager socket_manager_client;
 
-	// Black sqares
-	sf::RectangleShape sq[32];
-	for (int i = 0; i < 32; ++i) {
+	socket_manager_client.start(is_client ? 8012 : 9001, sf::IpAddress({ 127, 0, 0, 1 }));  // клиент должен подключаться к серверу на одном порту
+	socket_manager_server.start(is_client ? 9001 : 8012);  // сокета два => два порта
 
-		sq[i].setSize({ SQ_SIZE, SQ_SIZE });
-		sq[i].setFillColor(black_sq);
-		sq[i].setOutlineThickness(-2);
-		sq[i].setOutlineColor(black_sq);
 
+	// set color
+	bool MY_COLOR;
+	if (!is_client) {
+		MY_COLOR = rand() % 2;
+
+		Message message{
+					  .x_from = !MY_COLOR,
+					  .y_from = 0,
+					  .x_to = 0,
+					  .y_to = 0,
+					  .state = 2
+		};  
+		socket_manager_client.send_message(message);  // sent other color
 	}
-	int k = 0;
-	for (int i = 0; i < 8; ++i) {
-		for (int j = 0; j < 4; ++j) {
+	else {
+		while (true) {
 
-			sq[k].setPosition(calculate_checker_position(i, j));
-			++k;
+			if (socket_manager_server.is_message_received()) {  
+				Message received_message = socket_manager_server.get_received_message();  
+				
+				MY_COLOR = received_message.x_from;
+			}
+
 		}
 	}
 
-	// White checkers
-	sf::CircleShape w_checker[12];
-	for (int i = 0; i < 12; ++i) {
-
-		w_checker[i].setRadius(CHECKER_RADIUS);
-		w_checker[i].setFillColor(wite_checker);
-		w_checker[i].setOutlineThickness(-1);
-		w_checker[i].setOutlineColor(white_sq);
-
-	}
-	k = 0;
-	for (int i = 0 + pos; i < 3 + pos; ++i) {
-		for (int j = 0; j < 4; ++j) {
-
-			w_checker[k].setPosition(calculate_checker_position(i, j) + sf::Vector2f{ CHECKER_OFFSET, CHECKER_OFFSET });
-			++k;
-		}
-	}
+	int pos = MY_COLOR ? 5 : 0;  
+	set_my_color(MY_COLOR); // set to storage.cpp
 
 
-	// Black checkers
-	sf::CircleShape b_checker[12];
-	for (int i = 0; i < 12; ++i) {
 
-		b_checker[i].setRadius(CHECKER_RADIUS);
-		b_checker[i].setFillColor(black_checker);
-		b_checker[i].setOutlineThickness(-1);
-		b_checker[i].setOutlineColor(outline);
 
-	}
-	k = 0;
-	for (int i = 5 - pos; i < 8 - pos; ++i) {
-		for (int j = 0; j < 4; ++j) {
+	// Field screen ============================================================================================================================================================
+	sf::RectangleShape field({ FIELD_SIZE, FIELD_SIZE }); // Field (white sqares)
+	sf::RectangleShape sq[32];		// Black sqares
+	sf::CircleShape w_checker[12];	// White checkers
+	sf::CircleShape b_checker[12];	// Black checkers
 
-			b_checker[k].setPosition(calculate_checker_position(i, j) + sf::Vector2f{ CHECKER_OFFSET, CHECKER_OFFSET });
-			++k;
-
-		}
-	}
+	seting_field(field, sq);
+	setting_chekers(pos, w_checker, b_checker);
 
 
 	// binding checkers and sqares
-	k = 0;
+	int k = 0;
 	for (int i = 0 + pos; i < 3 + pos; ++i) {                //y
 		for (int j = 0; j < 8; ++j) {                        //x
 			if ((i + j) % 2 == 1) {
@@ -146,51 +115,91 @@ int main() {
 		}
 	}
 
-	//set color 
+
+
+
+
+
+
+	
 	
 
-
-
-
-
-	while (window.isOpen()) { //    Window    =================================================================
-
-
+	while (window.isOpen()) { //    Window    ==================================================================================================================================
 		while (const std::optional event = window.pollEvent()) {  // pollEvent - возвращает событие, если оно ожидает обработки
-			
+
 			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
 
 
-			// tape
+			// type ======================================================================================================================================================
 			if (const auto* mouseButton = event->getIf<sf::Event::MouseButtonPressed>()) {
 
 				int click_x = mouseButton->position.x;
 				int click_y = mouseButton->position.y;
 
 				// calculate point if click
-				sf::Vector2f point{ static_cast<float>(click_x), static_cast<float>(click_y)}; 
+				sf::Vector2f point{ static_cast<float>(click_x), static_cast<float>(click_y) };
 
-				if (field.getGlobalBounds().contains(point)) {
+				if (field.getGlobalBounds().contains(point) && !is_waiting_for) {
 
 					int x = (click_x - FIELD_OFFSET_X) / SQ_SIZE;
 					int y = (click_y - FIELD_OFFSET_Y) / SQ_SIZE;
 
-					type(x, y, sq, sqare);
+					if (sqare_type(x, y, sq, sqare)) {
+						is_waiting_for = true;
+
+						Message message{
+					  .x_from = last_x,
+					  .y_from = last_y,
+					  .x_to = x,
+					  .y_to = y,
+					  .state = 1};  
+
+						// send message
+						socket_manager_client.send_message(message);
+						std::cout << "Client sent message to ("<< last_x <<" " << last_y << " -> " << x << ", " << y << ")" << std::endl;
+					}
+					else {
+
+						last_x = x;
+						last_y = y;
+					}
+
+
 				}
+				if (is_waiting_for) {
+					
+					if (socket_manager_server.is_message_received()) {  
+						Message received_message = socket_manager_server.get_received_message(); 
+
+						if (received_message.state == 0) {
+							std::cout << "Error connection" << std::endl;
+							is_end = true;
+						}
+						if (received_message.state == 3) {
+							std::cout << "Error" << std::endl;
+							is_end = true;
+						}
+
+						std::cout << "[INFO] Received message: from (" << received_message.x_from << ", " << received_message.y_from << ") "
+							<< "to (" << received_message.x_to << ", " << received_message.y_to << ") "
+							<< "state: " << received_message.state << std::endl;  
+
+						// move
+						sqare_type(received_message.x_from, received_message.y_from, sq, sqare);
+						if (!sqare_type(received_message.x_to, received_message.y_to, sq, sqare)) { // incorrect move
+
+							std::cout << "ERROR" << std::endl;
+							is_end = true;
+							Message message{ .x_from = 0, .y_from = 0, .x_to = 0, .y_to = 0, .state = 3 };
+							socket_manager_client.send_message(message);
+							
+						}
 
 
-
-			}
-
-			
-			if (const auto* textEntered = event->getIf<sf::Event::TextEntered>()) {
-
-				if (textEntered -> unicode >= 0 && textEntered -> unicode <= 9)
-				std::cout << "ASCII character typed: " << static_cast<char>(textEntered->unicode) << std::endl;
-			
-
+					}
+				}
 			}
 		}
 
@@ -217,13 +226,107 @@ int main() {
 
 		}
 
-
-		
-
-
 		window.display();
 
 	}
 
 	return 0;
 }
+
+
+
+
+void seting_home_screen(sf::CircleShape& white_example, sf::CircleShape& black_example) {
+
+	white_example.setRadius(CHECKER_RADIUS * 3);
+	white_example.setFillColor(wite_checker);
+	white_example.setOutlineThickness(-1);
+	white_example.setOutlineColor(white_sq);
+	white_example.setPosition({ WINDOW_X / 2 - 100 - CHECKER_RADIUS * 6, WINDOW_Y / 2 - 100 });
+
+	black_example.setRadius(CHECKER_RADIUS * 3);
+	black_example.setFillColor(black_checker);
+	black_example.setOutlineThickness(-1);
+	black_example.setOutlineColor(outline);
+	black_example.setPosition({ WINDOW_X / 2 + 300 - CHECKER_RADIUS * 6, WINDOW_Y / 2 - 100 });
+}
+void seting_field(sf::RectangleShape& field, sf::RectangleShape sq[32]) {
+
+	field.setFillColor(white_sq);
+	field.setPosition({ FIELD_OFFSET_X, FIELD_OFFSET_Y });
+	field.setOutlineThickness(3);
+	field.setOutlineColor(outline);
+
+	// Black sqares
+	for (int i = 0; i < 32; ++i) {
+
+		sq[i].setSize({ SQ_SIZE, SQ_SIZE });
+		sq[i].setFillColor(black_sq);
+		sq[i].setOutlineThickness(-2);
+		sq[i].setOutlineColor(black_sq);
+
+	}
+	int k = 0;
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 4; ++j) {
+
+			sq[k].setPosition(calculate_checker_position(i, j));
+			++k;
+		}
+	}
+
+
+
+}
+void setting_chekers(int pos, sf::CircleShape w_checker[12], sf::CircleShape b_checker[12]) {
+
+	// White checkers
+	int k = 0;
+	for (int i = 0; i < 12; ++i) {
+
+		w_checker[i].setRadius(CHECKER_RADIUS);
+		w_checker[i].setFillColor(wite_checker);
+		w_checker[i].setOutlineThickness(-1);
+		w_checker[i].setOutlineColor(white_sq);
+
+	}
+	k = 0;
+	for (int i = 0 + pos; i < 3 + pos; ++i) {
+		for (int j = 0; j < 4; ++j) {
+
+			w_checker[k].setPosition(calculate_checker_position(i, j) + sf::Vector2f{ CHECKER_OFFSET, CHECKER_OFFSET });
+			++k;
+		}
+	}
+
+	// Black checkers
+	k = 0;
+	for (int i = 0; i < 12; ++i) {
+
+		b_checker[i].setRadius(CHECKER_RADIUS);
+		b_checker[i].setFillColor(black_checker);
+		b_checker[i].setOutlineThickness(-1);
+		b_checker[i].setOutlineColor(outline);
+
+	}
+	for (int i = 5 - pos; i < 8 - pos; ++i) {
+		for (int j = 0; j < 4; ++j) {
+
+			b_checker[k].setPosition(calculate_checker_position(i, j) + sf::Vector2f{ CHECKER_OFFSET, CHECKER_OFFSET });
+			++k;
+
+		}
+	}
+
+}
+
+
+/*
+message state:
+0 - exit
+1 - move
+2 - recive color
+3 - error
+4 - end 
+*/
+
